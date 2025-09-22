957cfa9e623bed54d83f38a74d1cdf5d0710dafa
Samaygupta18 <samaygupta054@gmail.comm>
2025-09-22 14:04:47 +0530

    adding /health route

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "UP", message: "User Service is running 🚀" });
+});
+
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
