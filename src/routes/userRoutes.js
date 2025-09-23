a6c64c7e3eebd0c9ffcda37df695abdb03d9e664
samaygupta052 <samaygupta054@gmail.com>
2025-09-23 15:31:58 +0530

    k8s workdone

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { getUserById } = require('../controllers/userController');
 
+router.get('/', (req, res) => {
+  res.send('User Service is running');
+});
+
 // Existing routes...
 router.get('/:id', getUserById);
 
